a5be517bbf15ca599d7adba315248bd73537d523
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-06 17:45:55 +0300

    [ADD] logger

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,12 @@
 import os
+import logging
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format=u"[%(asctime)s][%(filename)s][LINE:%(lineno)d][%(levelname)s] | %(message)s",
+    level=logging.INFO
+)
 
 
 class Config:
