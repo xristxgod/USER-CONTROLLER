669c6e6cd68be87fe9c41a92630ee5950b1ad254
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-06 17:36:48 +0300

    [ADD] orm and db

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from tortoise import Tortoise
 
 from .endpoints import router
 
@@ -9,6 +10,18 @@ main_router = APIRouter()
 main_router.include_router(router)
 
 
+async def init_db():
+    # Here we connect to a SQLite DB file.
+    # also specify the app name of "models"
+    # which contain models from "app.models"
+    await Tortoise.init(
+        db_url='sqlite://db.sqlite3',
+        modules={'models': ['src.models']}
+    )
+    # Generate the schema
+    await Tortoise.generate_schemas()
+
+
 __all__ = [
-    "main_router"
+    "main_router", "init_db"
 ]
---
+++ backend/src/models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from tortoise import fields
+from tortoise.models import Model
+
+
+class UserModel(Model):
+    user_id = fields.CharField(max_length=12, pk=True, unique=True)
+    name = fields.CharField(max_length=50)
+    surname = fields.CharField(max_length=50)
+    patronymic = fields.CharField(max_length=50, default=None, null=True)
+    phone_number = fields.CharField(max_length=11, unique=True)
+    email = fields.CharField(max_length=255, unique=True, default=None, null=True)
+    country = fields.CharField(max_length=50)
+    date_modified = fields.DatetimeField(default=datetime.now())
+    date_created = fields.DatetimeField(default=datetime.now())
