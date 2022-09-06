0b67eff77e6bd04970c369cbc83ecbb0aa8439f1
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-06 14:31:06 +0300

    [ADD] schemas

---
+++ backend/src/schemas.py
@@ -0,0 +1,92 @@
+import re
+from typing import Optional
+
+from pydantic import BaseModel, Field, EmailStr, constr, validator
+
+
+class BodySaveUser(BaseModel):
+    name: constr(max_length=50) = Field(description="Имя пользователя")
+    surname: constr(max_length=50) = Field(description="Фамилия пользователя")
+    patronymic: Optional[constr(max_length=50)] = Field(description="Отчество пользователя")
+    phoneNumber: constr(min_length=11, max_length=11) = Field(description="Телефонный номер", alias="phone_number")
+    email: Optional[EmailStr] = Field(description="Email")
+    country: constr(max_length=50) = Field(description="Старана")
+
+    @validator("name", "surname", "patronymic", "country")
+    def valid_fio(cls, value: Optional[str] = None):
+        if value is None:
+            return None
+        if bool(re.fullmatch(r'(?i)[а-яё ]+', value)):
+            raise ValueError("Only Cyrillic is allowed for the string!")
+        return value
+
+    @validator("phoneNumber")
+    def valid_phone_number(cls, number: str):
+        if not number.isdigit():
+            raise ValueError("The number must consist of integers. Example: 79281304521")
+        if number[0] == "7":
+            raise ValueError("The number must start with 7. Example: 79281304521")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Иван",
+                "surname": "Иванов",
+                "patronymic": "Иванович",
+                "phone_number": "79281304521",
+                "email": "ivanov@mail.ru",
+                "country": "Россия"
+            }
+        }
+
+
+class BodyNumber(BaseModel):
+    phoneNumber: constr(min_length=11, max_length=11) = Field(description="Телефонный номер", alias="phone_number")
+
+    @validator("phoneNumber")
+    def valid_phone_number(cls, number: str):
+        if not number.isdigit():
+            raise ValueError("The number must consist of integers. Example: 79281304521")
+        if number[0] == "7":
+            raise ValueError("The number must start with 7. Example: 79281304521")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "phone_number": "79281304521"
+            }
+        }
+
+
+class ResponseGetUser(BaseModel):
+    name: constr(max_length=50) = Field(description="Имя пользователя")
+    surname: constr(max_length=50) = Field(description="Фамилия пользователя")
+    patronymic: Optional[constr(max_length=50)] = Field(description="Отчество пользователя", default=None)
+    phoneNumber: constr(min_length=11, max_length=11) = Field(description="Телефонный номер", alias="phone_number")
+    email: Optional[EmailStr] = Field(description="Email", default=None)
+    country: constr(max_length=50) = Field(description="Старана")
+    countryCode: int = Field(description="Код страны", alias="country_code")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Иван",
+                "surname": "Иванов",
+                "patronymic": "Иванович",
+                "phone_number": "79281304521",
+                "email": "ivanov@mail.ru",
+                "country": "Россия",
+                "country_code": 12
+            }
+        }
+
+
+class ResponseSuccessfully(BaseModel):
+    successfully: bool = Field(description="Статус", default=True)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "successfully": True
+            }
+        }
