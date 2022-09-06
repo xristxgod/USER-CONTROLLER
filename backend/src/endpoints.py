41d3dfc0a2417fb2f9ef131c756ee289f51159c5
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-06 18:03:06 +0300

    [ADD] endpoints

--- backend/src/endpoints.py
+++ backend/src/endpoints.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+
+from .schemas import BodyUser, BodyNumber, ResponseSuccessfully, ResponseUser
+from .services import ServiceUserCRUD
 
 
 router = APIRouter(
@@ -6,19 +9,34 @@ router = APIRouter(
 )
 
 
-@router.post("/save_user_data")
-async def save_user():
-    pass
+@router.post(
+    "/save_user_data",
+    response_model=ResponseSuccessfully
+)
+async def save_user(body: BodyUser):
+    return await ServiceUserCRUD.create(body=body)
 
 
-@router.post("/get_user_data")
-async def get_user():
-    pass
+@router.post(
+    "/get_user_data",
+    response_model=ResponseUser
+)
+async def get_user(body: BodyNumber):
+    result = await ServiceUserCRUD.get(phone_number=body.phoneNumber)
+    if result is None:
+        raise HTTPException(detail="User with given phone number not found!", status_code=status.HTTP_404_NOT_FOUND)
+    return result
 
 
-@router.post("/delete_user_data")
-async def delete_user():
-    pass
+@router.post(
+    "/delete_user_data",
+    response_model=ResponseSuccessfully
+)
+async def delete_user(body: BodyNumber):
+    result = await ServiceUserCRUD.delete(phone_number=body.phoneNumber)
+    if result is None:
+        raise HTTPException(detail="User with given phone number not found!", status_code=status.HTTP_404_NOT_FOUND)
+    return result
 
 
 __all__ = [
