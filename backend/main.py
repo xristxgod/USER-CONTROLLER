3a2f43792f104b02b1c79ba5e1e09ab90a1c0378
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-07 09:34:00 +0300

    [ADD] close database

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from tortoise import Tortoise
 
-from src import main_router, init_db
+from src import main_router
+from config import Config
 
 
 app = FastAPI(
@@ -13,7 +15,13 @@ app.include_router(main_router)
 
 @app.on_event("startup")
 async def startup():
-    await init_db()
+    await Tortoise.init(db_url=Config.DATABASE_URI, modules={'models': ['src.models']})
+    await Tortoise.generate_schemas()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await Tortoise.close_connections()
 
 
 if __name__ == '__main__':
